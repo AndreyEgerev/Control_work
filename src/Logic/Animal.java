965e70c25db873032d27371ab51ced2ec41dc36a
Andrey <a.egerev@gmail.com>
2024-02-18 18:57:36 +0300

    Add new classes Animal, ListAnimals

--- src/Logic/Animal.java
+++ src/Logic/Animal.java
@@ -9,4 +9,21 @@ public class Animal {
     private LocalDate dateOfBirth;
     private HashSet<String> commands;
 
+    public Animal(String nameAnimal, String typeAnimal, LocalDate dateOfBirth, HashSet<String> commands) {
+        this.nameAnimal = nameAnimal;
+        this.typeAnimal = typeAnimal;
+        this.dateOfBirth = dateOfBirth;
+        this.commands = commands;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal" +
+                "name= '" + nameAnimal + '\'' +
+                ", type= '" + typeAnimal + '\'' +
+                ", Date of birth= " + dateOfBirth +
+                ", Execute commands= " + commands ;
+    }
+
+    public int
 }
