06e74ec1e8a97457b18d84e4107af97088b787f4
Andrey <a.egerev@gmail.com>
2024-02-18 17:26:46 +0300

    Add new classes Animal, ListAnimals

---
+++ src/Logic/Animal.java
@@ -0,0 +1,12 @@
+package Logic;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+public class Animal {
+    private String nameAnimal;
+    private String typeAnimal;
+    private LocalDate dateOfBirth;
+    private HashSet<String> commands;
+
+}
---
+++ src/Logic/ListAnimals.java
@@ -0,0 +1,8 @@
+package Logic;
+
+import java.util.ArrayList;
+
+public class ListAnimals {
+    private ArrayList<Animal> registry;
+    private String typeAnimals;
+}
